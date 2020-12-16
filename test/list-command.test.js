e503c8e0dada047d83c057f37c29d4a699236242
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-12-16 13:07:12 +0100

    test(list command): cover all scenarios

--- test/list-command.test.js
+++ test/list-command.test.js
@@ -1,28 +1,95 @@
 const path = require('path')
 const spawn = require('cross-spawn')
 
-describe('Command: list', () => {
-    it('should replace the pre 31.1.0 d2 package path with the updated one', () => {
-        const result = spawn.sync(
-            path.join(__dirname, '..', 'bin', 'd2-utils-codemods'),
-            ['list'],
-            { cwd: __dirname }
-        )
+const list = (options = []) => {
+    const result = spawn.sync(
+        path.join(__dirname, '..', 'bin', 'd2-utils-codemods'),
+        ['list', ...options],
+        { cwd: __dirname }
+    )
 
-        if (result.stderr.toString()) {
-            console.log(result.stderr.toString())
-        }
+    if (result.stderr.toString()) {
+        console.log('STDOUT:')
+        console.log(result.stdout.toString())
+        console.log('STDERR:')
+        console.log(result.stderr.toString())
+    }
+
+    return result
+}
+
+/*
+ * This is to handle the output awkwardness without having to deal with it in
+ * the test cases, as this could potentially change any time, depending on what
+ * the command uses to print the lines
+ */
+const output = lines => {
+    let newLines = lines
+
+    newLines = lines.map(line => `${line} `)
+    newLines = [...newLines, '']
+
+    return newLines.join('\n')
+}
+
+describe('Command: list', () => {
+    it('show all available codemods', () => {
+        const result = list()
 
         expect(result.stdout.toString()).toBe(
-            [
-                'module1 ',
-                '* module1:foo.js ',
-                '* module1:rename-foo.js ',
-                ' ',
-                'module2 ',
-                '* module2:bar.js ',
+            output([
+                'module1',
+                '* module1:foo.js',
+                '* module1:rename-foo.js',
                 '',
-            ].join('\n')
+                'module2',
+                '* module2:bar.js',
+                '',
+                'module3',
+                '* module3:foobar.js',
+            ])
+        )
+    })
+
+    it('should show only the codemods of the supplied packages', () => {
+        const result = list(['module1', 'module2'])
+
+        expect(result.stdout.toString()).toBe(
+            output([
+                'module1',
+                '* module1:foo.js',
+                '* module1:rename-foo.js',
+                '',
+                'module2',
+                '* module2:bar.js',
+            ])
+        )
+    })
+
+    it('should show only the codemods with the name option in its name', () => {
+        const result = list(['--name', 'bar'])
+
+        expect(result.stdout.toString()).toBe(
+            output([
+                'module2',
+                '* module2:bar.js',
+                '',
+                'module3',
+                '* module3:foobar.js',
+            ])
+        )
+    })
+
+    it('should show only the codemods with the name option in its name of the supplied package', () => {
+        const result = list(['module2', '--name', 'bar'])
+
+        expect(result.stdout.toString()).toBe(
+            // Do not use a single line array to mimic actual output lines
+            // eslint-disable-next-line prettier/prettier
+            output([
+                'module2',
+                '* module2:bar.js',
+            ])
         )
     })
 })
